9dc3ed431875483b7671120c5720f74d0fc29bcc
nhnajmul17 <nhnajmul17@gmail.com>
2021-10-04 15:05:20 +0600

    rating added

--- src/components/Feature/Feature.js
+++ src/components/Feature/Feature.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
+import Rating from 'react-rating';
 import { useHistory } from 'react-router';
-
 const Feature = (props) => {
 
     const history = useHistory()
     const handleSeeCourse = () => {
         history.push('./services')
     }
-    const { Title, picture } = props.course
+    const { Title, picture, rating } = props.course
     return (
         <div>
             <Col>
                 <Card className='h-100'>
                     <div className='d-flex justify-content-center '>
-                        <div>
-                            <Card.Img variant="top" className='h-100' src={picture} />
+                        <div className='p-4'>
+                            <Card.Img variant="top" className='h-100 w-100' src={picture} />
 
                         </div>
-                        <div>
+                        <div className='m-2'>
                             <Card.Body>
                                 <Card.Title className='fs-4 text-success'>{Title}</Card.Title>
 
                                 <Card.Text>
+                                    <Rating
+                                        className='text-warning'
+                                        initialRating={rating}
+                                        readonly
+                                        emptySymbol="far fa-star"
+                                        fullSymbol="fas fa-star"
+                                    />
                                     <Button variant='warning' onClick={handleSeeCourse} className='h-25 w-100'>See Course</Button>
                                 </Card.Text>
                             </Card.Body>
